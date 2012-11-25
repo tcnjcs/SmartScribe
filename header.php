789a8e2259e84a079d5a86edfb1e714f75d56779
Dan <kelner.dc@gmail.com>
2012-11-25 18:53:30 -0500

    first commit

---
+++ header.php
@@ -0,0 +1,25 @@
+<!-- Include this file on each page -->
+
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/page_style.css" rel="stylesheet" type="text/css" />
+
+<body>
+	<div id="header_image">
+        <div id="header"><h1><a></a></h1></div>
+    </div>
+
+	<table align="center">
+		<td align="center">
+		<ul id="list_nav">
+			<li><a href="./index.php">Home</a></li>
+			<li><a href="./transcribe.php">Transcribe Audio</a></li>
+			<li><a href="./upload.php">Upload File</a></li>
+			<li><a href="./browse.php">Browse Transciptions</a></li>
+			<li><a href="./account.php">Account</a></li>
+		</ul>
+		</td>
+	</table>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- Home page.  Pretty blank right now. -->
+
+<html>
+<?php include "./header.php"; ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<head>
+		<title>SmartScribe</title>
+		<link href="css/page_style.css" rel="stylesheet" type="text/css" />
+		<!--<style>
+			table {width:100%; border:1px black;}
+			th {background-color:lightgray;}
+		</style>-->
+		<script>
+		
+		</script>
+	</head>
+	
+	<body>
+	
+	</body>
+
+</html>
